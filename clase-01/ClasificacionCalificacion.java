15b6354c7564de3e333cded07fd42bba52d2c7eb
Nahuel Mioni <180794725+nahuelmioni@users.noreply.github.com>
2026-08-10 17:01:52 -0300

    ejercicio 1

---
+++ clase-01/ClasificacionCalificacion.java
@@ -0,0 +1,18 @@
+public class ClasificacionCalificacion {
+ public static void main(String[] args) {
+ int puntaje = 76;
+ char calificacion;
+ if (puntaje >= 90) {
+ calificacion = 'A';
+ } else if (puntaje >= 80) {
+ calificacion = 'B';
+ } else if (puntaje >= 70) {
+ calificacion = 'C';
+ } else if (puntaje >= 60) {
+ calificacion = 'D';
+ } else {
+ calificacion = 'F';
+ }
+ System.out.println("Calificacion = " + calificacion);
+ }
+}
